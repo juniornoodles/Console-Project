b043488020f63882aa8dbbd154d40001327b6cf6
juniornoodles <juniortchapmou@gmail.com>
2026-01-09 21:05:36 -0500

    Commenting

--- cpu_hardware/cpu.sv
+++ cpu_hardware/cpu.sv
@@ -68,8 +68,6 @@ logic write_en_in;
 logic write_en;
 logic [31:0] operand1_in;
 logic [31:0] operand2_in;
-logic [31:0] registers [9:0];
-logic [31:0] current; // for testing
 logic hazard1;
 logic hazard2;
 logic [31:0] forward_data1;
@@ -85,7 +83,6 @@ logic [12:0] pc_ID;
 logic [12:0] pc_EX;
 logic halted;
 logic debug;
-assign current = pc_mem[pc]; // for testing
 alu alu_inst(
     .reg1(operand1),
     .reg2(operand2),
@@ -101,17 +98,7 @@ reg_file reg_file_inst(
     .rd1_addr(instruction[4:0] != SW  ? instruction[14:10] : instruction[9:5]), //Checks if it is a store to get contents from rd reg
     .rd2_addr(instruction[19:15]),
     .rd1_data(reg_read_addr1),
-    .rd2_data(reg_read_addr2),
-    .reg1(registers[0]), //for testing
-    .reg2(registers[1]),
-    .reg3(registers[2]),
-    .reg4(registers[3]),
-    .reg5(registers[4]),
-    .reg6(registers[5]),
-    .reg7(registers[6]),
-    .reg8(registers[7]),
-    .reg9(registers[8]),
-    .reg10(registers[9])
+    .rd2_data(reg_read_addr2)
 );
 Hazard_unit hazard_unit_inst(
     .clk(clk),
@@ -154,25 +141,25 @@ Decode_To_Execute decode_to_execute_inst(
     .reset(reset),
     .stall(stall),
     .flush(flush),
-    .operand1_in(operand1_in), // Connect appropriately
-    .operand2_in(operand2_in), // Connect appropriately
+    .operand1_in(operand1_in), 
+    .operand2_in(operand2_in),
     .rd_in(instruction[9:5]),
-    .alu_op_in(instruction[4:0]),   // Connect appropriately
-    .operand1_out(decode_result1),// Connect appropriately
-    .operand2_out(decode_result2),// Connect appropriately
-    .alu_op_out(alu_op),   // Connect appropriately
+    .alu_op_in(instruction[4:0]),   
+    .operand1_out(decode_result1),
+    .operand2_out(decode_result2),
+    .alu_op_out(alu_op),   
     .rd_out(rd),
     .halted(halted)
 );
 Execute_To_Memory execute_to_memory_inst(
     .clk(clk),
     .reset(reset),
-    .alu_result_in(alu_op != JAL && alu_op != JALR ? alu_result : pc_EX), // Connect appropriately
+    .alu_result_in(alu_op != JAL && alu_op != JALR ? alu_result : pc_EX), 
     .rd_in(rd),
     .memaddr_in(operand2[16:0]),
     .alu_op_in(alu_op),
-    .alu_result_out(memdata), // Connect appropriately
-    .memaddr_out(memaddr),         // Connect appropriately
+    .alu_result_out(memdata), 
+    .memaddr_out(memaddr),         
     .rd_out(writeback_regaddr_in),
     .alu_op_out(mem_alu_op),
     .halted(halted)
@@ -183,9 +170,9 @@ always_ff @(posedge clk, posedge reset) begin
         pc <= START_OF_PROGRAM;
     end else if (flush) begin
         if(alu_op == JALR) begin
-            pc <= alu_result[12:0];
+            pc <= alu_result[12:0]; //Whatever is in the register plus the immidate value for the jump address
         end else begin
-            pc <= pc + not_predicted_offset;
+            pc <= pc + not_predicted_offset; //If a flush is needed then the wrong branch outcome was predicted
         end
     end else if (stall | halted) begin
         pc <= pc;
@@ -194,7 +181,7 @@ always_ff @(posedge clk, posedge reset) begin
     end
 end
 
-always_ff @(posedge clk) begin
+    always_ff @(posedge clk) begin //Here just so JAL and JALR can have the program instruction to store in a register
     if (!stall & !halted) begin
         pc_ID <= pc + 1;
         pc_EX <= pc_ID;
@@ -205,7 +192,7 @@ always_ff @(posedge clk) begin
 end
 
 //Decode stage
-always_comb begin
+    always_comb begin  //If it is an R type then take the contents of the 2 read port, otherwise its an immediate, take it from he instruction itself. Contains sign extending logic.
     operand2_in = (instruction[4:0] <= R_TYPE ? reg_read_addr2 : (instruction[4:0] == ADDI | instruction[4:0] == ILT | instruction[4:0] == ILTI | instruction[4:0] == SLOG | instruction[4:0] == SLOGI | instruction[4:0] == SARI | instruction[4:0] == SARII | instruction[4:0] == BT | instruction[4:0] == BF | instruction[4:0] == JALR) & instruction[31] == 1 ? {{15{1'b1}},instruction[31:15]} : {{15{1'b0}},instruction[31:15]});
     operand1_in = reg_read_addr1;
 end
@@ -213,12 +200,12 @@ end
 
 //Execute stage
 assign operand1 = hazard1 ? forward_data1 : decode_result1;
-assign operand2 = (alu_op == BT | alu_op == BF) ? 32'b0 : hazard2 ? forward_data2 : decode_result2;
+    assign operand2 = (alu_op == BT | alu_op == BF) ? 32'b0 : hazard2 ? forward_data2 : decode_result2; // If a branch instruction, set the second operand to 0 to check if it equals zero or not in the alu
 always_comb begin
     if(alu_op == JALR) begin
         flush = 1'b1;
     end else if((alu_op == BT | alu_op == BF)) begin
-        if(decode_result2[31] ^ alu_result[0]) begin
+        if(decode_result2[31] ^ alu_result[0]) begin // With BTFNT prediction, if the offset is negative and the alu determines a branch shouldn't happen the predicition is wrong and vice versa
             flush = 1'b1;
         end else begin
             flush = 1'b0;
@@ -228,7 +215,7 @@ always_comb begin
     end
 end
 
-always_ff @(posedge clk or posedge reset or posedge finish_debug) begin
+    always_ff @(posedge clk or posedge reset or posedge finish_debug) begin 
     if(reset | (debug & finish_debug)) begin
         debug = 1'b0;
     end else if(alu_op == EBREAK) begin
@@ -266,9 +253,9 @@ end
 //debuging
 always_ff @(posedge clk or posedge reset) begin
     if(reset | !debug) begin
-        halted <= 1'b0;
+        halted <= 1'b0; 
     end else begin
-        halted <= 1'b1;
+        halted <= 1'b1; //If debug is on then everything halts, if the pause signal is off then the program goes through one cycle.
         if(!pause) begin
             halted <= 1'b0;
         end
@@ -283,114 +270,8 @@ initial begin
     for (i = 0; i < RAM_SIZE; i = i + 1) begin
         RAM[i] = 32'b0;
     end 
-    RAM[0] = 32'd74;
-    pc_mem[0] = {17'd0,5'd0,5'd1,LW};
-    pc_mem[1] = {17'd10,5'd1,5'd3,EBREAK};
-    pc_mem[2] = {17'd17,5'd0,5'd6,JAL};
-    pc_mem[3] = {17'd2,5'd2,5'd3,ADDI};
-    pc_mem[4] = {17'd0,5'd3,5'd4,ILTI};
-    pc_mem[19] = {17'd21,5'd0,5'd3,LI};
-    pc_mem[20] = {17'd4,5'd2,5'd8,ADDI};
-    pc_mem[21] = {17'd1,5'd5,5'd10,SLOGI};
-    pc_mem[22] = {17'd2,5'd6,5'd7,JALR};
-    pc_mem[23] = {17'b11111111111111110,5'd4,5'd5,BT};
-
-
-
-    #10; 
-    $monitor(
-        "PC: %d \n \
-        Newest Instruction %b: \n \
-\n \
-\n \
-        Reg 1: %b \n \
-        Reg 2: %b \n \
-        Reg 3: %b \n \
-        Reg 4: %b \n \
-        Reg 5: %b \n \
-        Reg 6: %b \n \
-        Reg 7: %b \n \
-        Reg 8: %b \n \
-        Reg 9: %b \n \
-        Reg 10: %b \n \
-        RAM[0]: %b \n \
-\n \
-\n \
-        Fetch to Decode Stage: Instruction: %b \n \
-        Decode to Execute Stage: Op1: %b Op2: %b ALU_OP: %b RD: %d Hazard1: %b Hazard2: %b \n \
-        Execute to Memory Stage: ALU_RESULT: %d MEMADDR: %b RD: %d \n \
-        Memory to Writeback Stage: Writeback data: %b Writeback addr: %d \
-        \n \
-        -------------------------------\n",
-        pc,
-        current,
-        registers[0],
-        registers[1],
-        registers[2],
-        registers[3],
-        registers[4],
-        registers[5],
-        registers[6],
-        registers[7],
-        registers[8],
-        registers[9],
-        RAM[0],
-        instruction,
-        operand1, operand2, alu_op, rd, hazard1, hazard2,
-        memdata, memaddr, writeback_regaddr_in,
-        writeback_data, writeback_regaddr);
-    // Example instruction
-    // Add more initialization as needed
+    // Either put a file here to read from or manually enter the contents of the PC memory and the RAM
 end
-
-
 endmodule
 
-module tb;
 
-    logic clk;
-    logic reset;
-    logic pause;
-    logic finish_debug;
-    cpu cpu_inst(
-        .clk(clk),
-        .reset(reset),
-        .pause(pause),
-        .finish_debug(finish_debug)
-    );
-
-
-    initial begin
-        $dumpfile("cpu.vcd");
-        $dumpvars(0, tb);
-        clk = 0;
-        reset = 1;
-        pause = 1;
-        finish_debug = 0;
-        #10;
-        reset = 0;
-        repeat (15) begin
-            #5 clk = ~clk;
-            #5 clk = ~clk;
-        end
-        pause = 0;
-        repeat (4) begin
-            #5 clk = ~clk;
-            #5 clk = ~clk;
-        end
-        pause = 1;
-        repeat (40) begin
-            #5 clk = ~clk;
-            #5 clk = ~clk;
-        end
-        finish_debug = 1;
-        #10;
-        finish_debug = 0;
-        repeat (10) begin
-            #5 clk = ~clk;
-            #5 clk = ~clk;
-        end
-
-
-    end
-endmodule
