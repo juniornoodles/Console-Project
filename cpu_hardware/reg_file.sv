a7f3707c75e9f5945a41a8b9d29f791b2b373245
juniornoodles <juniortchapmou@gmail.com>
2026-02-16 15:05:05 -0500

    Update reg_file.sv
    Made register 31 memory mapped IO for the GPIO buttons on FPGA.

--- cpu_hardware/reg_file.sv
+++ cpu_hardware/reg_file.sv
@@ -11,24 +11,47 @@ module reg_file (
     input logic [4:0] wr_addr,
     input logic [31:0] wr_data,
     input logic write_en,
+    input logic up,
+    input logic down,
+    input logic left,
+    input logic right,
+    input logic select,
     output logic [31:0] rd1_data,
     output logic [31:0] rd2_data
 );
-assign rd1_data = rd1_addr == 0 ? 32'b0 : rd1_addr == wr_addr & write_en ? wr_data : registers[rd1_addr];
-assign rd2_data = rd2_addr == 0 ? 32'b0 : rd2_addr == wr_addr & write_en ? wr_data : registers[rd2_addr];
-logic [31:0] registers [31:0];
+localparam NONE = 3'd0,
+           UP = 3'd1,
+           DOWN = 3'd2,
+           LEFT = 3'd3,
+           RIGHT = 3'd4,
+           SELECT = 3'd5;
+logic [31:0] registers [30:0];
+logic [31:0] input_reg;
+assign rd1_data = rd1_addr == 0 ? 32'b0 : rd1_addr == 5'd31 ? input_reg : rd1_addr == wr_addr & write_en ? wr_data : registers[rd1_addr];
+assign rd2_data = rd2_addr == 0 ? 32'b0 : rd2_addr == 5'd31 ? input_reg : rd2_addr == wr_addr & write_en ? wr_data : registers[rd2_addr];
 always_ff @(posedge clk or posedge reset) begin
     if (reset) begin
         for (int i = 0; i < 32; i++) begin
             registers[i] <= 32'b0;
         end
-       
     end else if (write_en) begin
         registers[wr_addr] <= wr_data;
     end
 end
 
+always_comb begin
+    if(select) begin
+        input_reg = SELECT;
+    end else if (up) begin
+        input_reg = UP;
+    end else if (down) begin
+        input_reg = DOWN;
+    end else if (left) begin
+        input_reg = LEFT;
+    end else if (right) begin
+        input_reg = RIGHT;
+    end else begin
+        input_reg = NONE;
+    end
+end
 endmodule
-
-
-
