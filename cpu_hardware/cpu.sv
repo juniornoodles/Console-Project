361dfbbe7755b19570359f5c04d2570272efe711
juniornoodles <juniortchapmou@gmail.com>
2026-02-21 20:56:31 -0500

    Bug fix
    fixed bug where LI wouldnt sign extend

--- cpu_hardware/cpu.sv
+++ cpu_hardware/cpu.sv
@@ -293,7 +293,7 @@ always_ff @(posedge clk) begin
             mem_data_out <= 32'b0;
             RAM[memaddr] <= memdata;
         end else if(mem_alu_op == LI) begin
-            mem_data_out <= {{15{1'b0}},memaddr};
+            mem_data_out <= {{15{memaddr[16]}},memaddr};
         end else if(mem_alu_op == LUI) begin
             mem_data_out <= {memaddr[14:0],17'b0};
         end else begin
@@ -400,6 +400,7 @@ initial begin
 end
 endmodule
 */
+
 
 
 
